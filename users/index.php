b46d6eace16a24bc52c23dcce465e1ee51f6dc26
Tasha <teaton@paifashion.com>
2017-04-05 13:53:55 -0500

    Directory listing

---
+++ users/index.php
@@ -0,0 +1,13 @@
+<?php 
+if ($handle = opendir('.')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "<a href="$entry">$entry</a><br>";
+        }
+    }
+
+    closedir($handle);
+}
